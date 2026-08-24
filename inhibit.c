334f30c3858775e4fe7670fd5d9cfa1c5dced721
Administrador <arnaldo.santos@inpe.br>
2026-08-24 20:24:45 -0300

    [2.0.1] Preparacao para arquivo de configuracao~

--- inhibit.c
+++ inhibit.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "comum.h"
 #include "esone.h"
+/*
+CCCI com true: Define (set) o sinal de Inhibit. Os módulos ficam bloqueados.
+CCCI com false: Limpa (reset) o sinal de Inhibit. Os módulos são liberados para trabalhar.
+*/
 
 void inhibt(int *valor){
     ccci(&ext, valor);
